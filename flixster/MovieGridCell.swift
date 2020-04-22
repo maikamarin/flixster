f2f5c8c7e4a33a855aac771393743046e7596580
maikamarin <56859141+maikamarin@users.noreply.github.com>
2020-04-21 18:25:51 -0700

    movie grid layout

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Maika Marin on 4/21/20.
+//  Copyright © 2020 Maika Marin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+}
